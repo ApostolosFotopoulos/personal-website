30886293c07337841a04ec80d70d5424f2fbc5f5
ApostolosFotopoulos <apfotopoulos1997@gmail.com>
2023-05-02 20:13:37 +0300

    added contact section, intergrated emailJS

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const hamburgerLineTop = document.querySelector(".hamburger-lines__line--top");
 const hamburgerLineMid = document.querySelector(".hamburger-lines__line--mid");
 const hamburgerLineBot = document.querySelector(".hamburger-lines__line--bot");
 const mobileMenu = document.querySelector(".mobile-menu");
-console.log(hamburgerLines);
 
 hamburgerLines.addEventListener("click", function () {
   hamburgerLineTop.classList.toggle("hamburger-lines__line--top-opened");
