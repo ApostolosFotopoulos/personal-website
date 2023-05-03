fa0b16074ba0986bff1095ca7be130029e0ddb4b
ApostolosFotopoulos <apfotopoulos1997@gmail.com>
2023-05-03 20:24:20 +0300

    fixed mobile menu

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const hamburgerLines = document.querySelector(".hamburger-lines");
-const hamburgerLineTop = document.querySelector(".hamburger-lines__line--top");
-const hamburgerLineMid = document.querySelector(".hamburger-lines__line--mid");
-const hamburgerLineBot = document.querySelector(".hamburger-lines__line--bot");
+const hamburgerLineTop = document.querySelector(".hamburger-lines__line-top");
+const hamburgerLineMid = document.querySelector(".hamburger-lines__line-mid");
+const hamburgerLineBot = document.querySelector(".hamburger-lines__line-bot");
 const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenuItems = document.querySelectorAll(".mobile-menu__ul-item");
 
 hamburgerLines.addEventListener("click", function () {
-  hamburgerLineTop.classList.toggle("hamburger-lines__line--top-opened");
-  hamburgerLineMid.classList.toggle("hamburger-lines__line--mid-opened");
-  hamburgerLineBot.classList.toggle("hamburger-lines__line--bot-opened");
+  hamburgerLineTop.classList.toggle("hamburger-lines__line-top--opened");
+  hamburgerLineMid.classList.toggle("hamburger-lines__line-mid--opened");
+  hamburgerLineBot.classList.toggle("hamburger-lines__line-bot--opened");
   mobileMenu.classList.toggle("mobile-menu--opened");
+});
+
+mobileMenuItems.forEach((menuItem) => {
+  menuItem.addEventListener("click", function () {
+    hamburgerLineTop.classList.toggle("hamburger-lines__line-top--opened");
+    hamburgerLineMid.classList.toggle("hamburger-lines__line-mid--opened");
+    hamburgerLineBot.classList.toggle("hamburger-lines__line-bot--opened");
+    mobileMenu.classList.toggle("mobile-menu--opened");
+  });
 });
